e76d0318d771e1ecf1163f6eb8f5061d635e30f8
moonrailgun <moonrailgun@gmail.com>
2025-02-22 23:25:51 +0800

    refactor: remove unused post model

--- app/src/server/db/schema.ts
+++ app/src/server/db/schema.ts
@@ -22,27 +22,6 @@ import { expoConfigSchema, expoMetadataSchema } from "../api/expo/schema";
  */
 export const createTable = pgTableCreator((name) => name);
 
-export const posts = createTable(
-  "post",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 255 }),
-    createdById: varchar("created_by", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date(),
-    ),
-  },
-  (example) => ({
-    createdByIdIdx: index("created_by_idx").on(example.createdById),
-    nameIndex: index("name_idx").on(example.name),
-  }),
-);
-
 export const users = createTable("user", {
   id: varchar("id", { length: 255 })
     .notNull()
