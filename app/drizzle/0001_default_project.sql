e3993d4d815fcc0983a4f1f4b0b45560ba97a112
moonrailgun <moonrailgun@gmail.com>
2025-02-27 22:03:55 +0800

    refactor: change default channel name to align eas

--- app/drizzle/0001_default_project.sql
+++ app/drizzle/0001_default_project.sql
@@ -4,4 +4,4 @@
 INSERT INTO "public"."project" ("id", "name") VALUES ('mxcv71ljj1l4elkqu0crv3i3', 'Default');
 
 -- Add default channel
-INSERT INTO "public"."channel" ("id", "project_id", "name") VALUES ('es48bumsin9fo1afor6on4c9', 'mxcv71ljj1l4elkqu0crv3i3', 'Production');
+INSERT INTO "public"."channel" ("id", "project_id", "name") VALUES ('es48bumsin9fo1afor6on4c9', 'mxcv71ljj1l4elkqu0crv3i3', 'production');
