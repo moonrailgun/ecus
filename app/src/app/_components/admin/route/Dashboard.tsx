f6a5838f1b60488cd45ea85e0dfee5695dca532f
moonrailgun <moonrailgun@gmail.com>
2025-03-04 04:03:36 +0800

    refactor: update dashboard

--- app/src/app/_components/admin/route/Dashboard.tsx
+++ app/src/app/_components/admin/route/Dashboard.tsx
@@ -1,28 +1,47 @@
 import React from "react";
-import { Card, Typography } from "tushan";
+import { Button, Card, Typography } from "tushan";
 import { useAdminStore } from "../useAdminStore";
+import Link from "next/link";
 
 export const AdminDashboard: React.FC = React.memo(() => {
   const projectId = useAdminStore((state) => state.projectId);
 
-  const updateUrl = `${window.origin}/api/${projectId}/manifest}`;
+  const updateUrl = `${window.origin}/api/${projectId}/manifest`;
+  const installCli = `npm install -g ecus-cli`;
 
   return (
     <div className="flex flex-col gap-2">
-      <h1>Dashboard</h1>
-
       <Card>
         <Typography.Title heading={6}>Project ID</Typography.Title>
-        <Typography.Text copyable={{ text: projectId }}>
+        <Typography.Text copyable={{ text: projectId }} code>
           {projectId}
         </Typography.Text>
       </Card>
 
       <Card>
-        <Typography.Title heading={6}>Update your update url</Typography.Title>
-        <Typography.Text copyable={{ text: updateUrl }}>
+        <Typography.Title heading={6}>
+          Update your update url in{" "}
+          <Typography.Text type="primary">app.json</Typography.Text>
+        </Typography.Title>
+        <Typography.Text copyable={{ text: updateUrl }} code={true}>
           {updateUrl}
         </Typography.Text>
+      </Card>
+
+      <Card>
+        <Typography.Title heading={6}>
+          Download <Typography.Text type="primary">ecus-cli</Typography.Text>
+        </Typography.Title>
+        <Typography.Text code={true} copyable={{ text: installCli }}>
+          {installCli}
+        </Typography.Text>
+      </Card>
+
+      <Card>
+        <Typography.Title heading={6}>Get your API key</Typography.Title>
+        <Link href="/admin/apikey">
+          <Button type="primary">Click here to get your API key</Button>
+        </Link>
       </Card>
     </div>
   );
